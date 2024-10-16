0b14bd0525c0eba5360ed0fc39beb62547ebda91
Yosuf Haydary <yosuf.haydary@koop.overheid.nl>
2024-10-16 16:12:23 +0200

    Move the current website to /archive

--- release.sh
+++ release.sh
@@ -7,4 +7,4 @@
 # Thanks to https://itnext.io/how-to-add-git-hash-into-your-angular-application-bec20b702bb1
 node grab-git-info.js
 
-ng build --configuration production --base-href "https://softwaredam.com"
+ng build --configuration production --base-href "https://softwaredam.com/archive"
